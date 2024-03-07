597cf086619a7f4e3ece8c3b97f76f6997bb379d
Matheus Evangelista <matheusmotorista070@gmail.com>
2024-03-07 10:23:56 -0300

    updt1023

---
+++ LAB03_APOS_AULA/ANTES_AULA/Autor.java
@@ -0,0 +1,23 @@
+// classe para montagem e exibição dos dados do autor do programa
+public class Autor {
+
+	// Uso geral - exibição de dados do autor do trabalho
+	// --------------------------------------------------
+	void exibeDadosAutor() {
+	
+		// identificação do autor do trabalho
+		System.out.println();
+		System.out.println("---------------------------------");
+		System.out.println("ESTRUTURAS DE DADOS - 2024/1S");
+		System.out.println("Lab.: STRINGS, ARRAYS, MATH");
+		System.out.println("---------------------------------");
+		System.out.println("Nome: MATHEUS EVANGELISTA RODRIGUES");
+		System.out.println("R.A.: 121 092 231 1 004");
+		System.out.println("---------------------------------");
+		System.out.println();
+	
+		// finalização (desnecessário neste caso, pois é procedimento 'void')
+		return;	
+	}
+    
+}

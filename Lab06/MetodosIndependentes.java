5368576a50200757dc41129b1adc9f6b00810998
MatheusDev007 <matheusmotorista070@gmail.com>
2024-04-04 09:08:02 -0300

    updt0907

--- Lab06/MetodosIndependentes.java
+++ Lab06/MetodosIndependentes.java
@@ -11,7 +11,7 @@ public class MetodosIndependentes {
     // método printArrayInt para imprimir um array de 'int'
     public void printArrayInt(int[] inputArray) {
         for (int element : inputArray) {
-            System.out.printf("%5s ", element);       // exibe elementos do array
+            System.out.printf("%5s ", element); // exibe elementos do array
         }
         System.out.println();
     } // fim do método printArrayInt
@@ -19,22 +19,29 @@ public class MetodosIndependentes {
     // método printArrayDouble para imprimir um array de 'double'
     public void printArrayDouble(double[] inputArray) {
         for (double element : inputArray) {
-            System.out.printf("%5s ", element);       // exibe elementos do array
+            System.out.printf("%5s ", element); // exibe elementos do array
         }
         System.out.println();
     } // fim do método printArrayDouble
 
     // método printArrayString para imprimir um array de 'String'
     // AQUI ...
+    public void printArrayString(String[] inputArray) {
+        for (String element : inputArray) {
+            System.out.printf("%5s ", element); // exibe elementos do array
+        }
+        System.out.println();
+    }// fim do método printArrayString
 
     // método buscaArrayInt para buscar um conteúdo ('key') num array de 'int'
-    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não encontrou
+    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não
+    // encontrou
     public int buscaArrayInt(int[] inputArray, int key) {
 
         // busca sobre o vetor
-        for( int i = 0; i < inputArray.length; i++ ) {
+        for (int i = 0; i < inputArray.length; i++) {
             // se encontrou, já pode acabar - retorna a posição i
-            if ( inputArray[i] == key ) {
+            if (inputArray[i] == key) {
                 return i;
             }
         }
@@ -42,19 +49,20 @@ public class MetodosIndependentes {
         // se terminou a busca sem encontrar, retorna -1
         return -1;
 
-    }  // fim do método buscaArrayInt
+    } // fim do método buscaArrayInt
 
     // método buscaArrayDouble para buscar um conteúdo ('key') num array de 'double'
-    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não encontrou
+    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não
+    // encontrou
     public int buscaArrayDouble(double[] inputArray, double key) {
 
         // tolerância: se a diferença estiver abaixo, os números são considerados iguais
         final double TOLERANCIA = 0.00001;
 
         // busca sobre o vetor
-        for( int i = 0; i < inputArray.length; i++ ) {
+        for (int i = 0; i < inputArray.length; i++) {
             // se encontrou, já pode acabar - retorna a posição i
-            if ( Math.abs(inputArray[i] - key) < TOLERANCIA) {
+            if (Math.abs(inputArray[i] - key) < TOLERANCIA) {
                 return i;
             }
         }
@@ -62,10 +70,11 @@ public class MetodosIndependentes {
         // se terminou a busca sem encontrar, retorna -1
         return -1;
 
-    }  // fim do método buscaArrayDouble
+    } // fim do método buscaArrayDouble
 
     // método buscaArrayString para buscar um conteúdo ('key') num array de 'String'
-    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não encontrou
+    // retorna a posição em que o conteúdo foi encontrado no array ou -1 se não
+    // encontrou
     // AQUI ...
 
 }
